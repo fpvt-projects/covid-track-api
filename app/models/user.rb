6bc971fb18b63ad445d0dcf12ac705632a926726
TanTanMa <gelatan12@gmail.com>
2022-01-27 20:49:39 +0800

    added validations for users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    validates :lastname, :firstname, length: {minimum:2}, presence: true
+
+    #Birthdate should no be greater than the date today, Use Date.parse("January 12 2000") to enter data
+    validates :birthdate, comparison: {less_than: Date.today}, presence: true
+
+    #Cellnumber should be unique, and only numerical string with length between 11 to 13 chars.
+    validates :cellnumber, length: {minimum: 11, maximum: 13}, format:{with: /\A[+-]?\d+\z/ }, presence: true, uniqueness: true
+
+    validates :gender, :address, :city, presence: true
 end
