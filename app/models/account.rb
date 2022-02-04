8225487bbdcfcfa6a57013a46cec2d5c5215b2d3
TanTanMa <gelatan12@gmail.com>
2022-02-04 19:05:04 +0800

    generated Accounts database

---
+++ app/models/account.rb
@@ -0,0 +1,6 @@
+class Account < ApplicationRecord
+    has_secure_password
+
+    validates :email, :password_digest, presence: true, uniqueness: true
+    validates :email, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+end
