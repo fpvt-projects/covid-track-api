6ed59df7b9760aa2e361b7c9678af07acb7be83f
TanTanMa <gelatan12@gmail.com>
2022-02-06 22:28:46 +0800

    Turned result_log into singular to pass the database associations requirements

--- app/controllers/account_token_controller.rb
+++ app/controllers/account_token_controller.rb
@@ -1,10 +1,10 @@
 class AccountTokenController < Knock::AuthTokenController 
     skip_before_action :verify_authenticity_token, raise: false
 
-    def create
-        render json: {
-        jwt: auth_token.token,
-        userDetails: Account.find(auth_token.payload[:sub])
-             }, status: :created
-        end
+    # def create
+    #     render json: {
+    #     jwt: auth_token.token,
+    #     userDetails: Account.find(auth_token.payload[:sub])
+    #          }, status: :created
+    #     end
 end
--- app/models/quarantine_log.rb
+++ app/models/quarantine_log.rb
@@ -1,4 +1,4 @@
 class QuarantineLog < ApplicationRecord
     belongs_to :user, dependent: :destroy
-    belongs_to :result_logs, dependent: :destroy
+    belongs_to :result_log, dependent: :destroy
 end
