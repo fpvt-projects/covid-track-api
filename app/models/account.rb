55052e96122ad6e4228c1e54f46fba37810bcf58
jerco2 <jericogodoy7296@gmail.com>
2022-02-08 20:18:09 +0800

    testbranch commit

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,4 +5,11 @@ class Account < ApplicationRecord
     validates :email, :password_digest, presence: true, uniqueness: true
     validates :email, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
 
+    # def to_token_payload
+    #     {:id => self.id, :email => self.email, :user_id => self.user.id}
+    # end
+
+    # def self.from_token_payload(payload)
+    #     self.find payload["sub"]
+    # end
 end
