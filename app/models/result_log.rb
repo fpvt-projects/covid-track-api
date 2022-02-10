ca5ca9e11c7c9f0527d075b04e889a1185b0c7af
TanTanMa <gelatan12@gmail.com>
2022-02-10 18:24:47 +0800

    fix the conditional creation and status of quarantine log

--- app/models/result_log.rb
+++ app/models/result_log.rb
@@ -3,51 +3,40 @@ class ResultLog < ApplicationRecord
 
     validates :brand, :result, :antigen_type, length: {minimum: 2}, presence: true
     validates :user_id, presence:true
+   
 
-
-    after_save :create_quarantine_log
+    after_create :will_quarantine
 
     #If user is positive and has no postive entry the past 7 days,
-    #>>Make new result and quarantine log for the last day of quarantine
+    #Quarantine status will be flagged as "Start" and the Date is the last day of quarantine
     #if user is currently under going quarantine, the new qlog status would be Ongoing, regardless of result of test
     #if user is not under going quaratine and has negative result, the quarantinelog status will be "Random"
-    def create_quarantine_log
-        has_entry = QuarantineLog.where("date > ?", Date.today)
 
-        if result == "Positive" && has_entry == false
-            r = ResultLog.new(
-                antigen_type: "Please post on designated date",
-                result: "Please post on designated date",
-                brand: "Please post on designated date",
-                user_id: self.user_id
+
+    def will_quarantine
+        check_entry = QuarantineLog.last_start.find_by(user_id: user_id)
+
+        if check_entry == nil && result == "Positive"
+            QuarantineLog.create(
+                status: "Start",
+                date: (Date.today + 7.days),
+                user_id: user_id,
+                result_log_id: id
             )
-            r.save
-                if r.save == false
-                    p r.error.full_messages
-                end
-            q = QuarantineLog.new(
-                status: "Ended",
-                date: Date.today + 7.days,
-                user_id: r.user_id,
-                result_log_id: r.id
-            )
-            q.save
-                if q.save == false
-                    p q.errors.full_messages
-                end
-        else
-            q = QuarantineLog.new(
-                status: "#{has_entry ? "Ongoing" : "Random Testing"}",
+        elsif check_entry.present? && check_entry.date == Date.today 
+            QuarantineLog.create(
+                status: "Ended", 
                 date: Date.today,
                 user_id: user_id,
                 result_log_id: id
             )
-            q.save
-            if q.save == false
-                p q.errors.full_messages
-            end
-
+        else
+            QuarantineLog.create(
+                status: "#{check_entry.present? && check_entry.date > Date.today ? "Ongoing" : "Random Testing"}", 
+                date: Date.today,
+                user_id: user_id,
+                result_log_id: id
+            )
         end
-
     end
 end
