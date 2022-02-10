703071798cfa3956d6802f20fbc27c90296b709f
TanTanMa <gelatan12@gmail.com>
2022-02-11 02:06:09 +0800

    fixed errors in creating new accounts

--- app/controllers/v1/registrations_controller.rb
+++ app/controllers/v1/registrations_controller.rb
@@ -1,21 +1,22 @@
 class V1::RegistrationsController < ApplicationController
     def create
         @account = Account.new(account_params)
+
         if @account.save
-            session[:account_id] = account.id
+            session[:account_id] = @account.id
             render json: {
                 status: :created,
-                account: account
+                account: @account
             }
         else
-            render json: {status: 500}
+            render json: {status: 500, errors: @account.errors.full_messages}
         end
     end
 
     private
 
     def account_params 
-        params.require(:account).permit(:email, :password, :password_confirmation)
+        params.require(:account).permit(:email, :password, :password_confimation)
     end
 
 end
