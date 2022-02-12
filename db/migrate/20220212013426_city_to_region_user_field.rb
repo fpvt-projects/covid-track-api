ec76307873ab206a94624c18763904e169c58658
TanTanMa <gelatan12@gmail.com>
2022-02-12 10:10:09 +0800

    err in changing dataype in heroku

--- db/migrate/20220212013426_city_to_region_user_field.rb
+++ db/migrate/20220212013426_city_to_region_user_field.rb
@@ -1,7 +1,7 @@
 class CityToRegionUserField < ActiveRecord::Migration[7.0]
   def change
     rename_column :users, :city, :region
-    change_column :users, :birthdate, :datetime, :date
+    change_column :users, :birthdate, :date
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
